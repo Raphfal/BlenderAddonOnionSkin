a069f573f8da4895ae70b0b1dfc9fd343fb99800
Your Name Raphaël <raphfalai@gmail.com>
2025-09-09 21:13:03 +0200

    script for the UI created

---
+++ code/UiOnionSkin3D.py
@@ -0,0 +1,19 @@
+bl_info = {
+    "name": "Onion Skin For 3D animation free",
+    "author": "H.Raph",
+    "version": (1, 0),
+    "blender": (4, 5, 3),
+    "location": "",
+    "description": "An onion skin for 3d animation",
+    "category": "",
+}
+
+
+import bpy
+
+*****
+To do UI of the addon
+
+
+
+*****
