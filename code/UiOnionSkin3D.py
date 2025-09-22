44a0f0bbfe5dd53705af1169585a9ea5d8bc333b
Your Name Raphaël <raphfalai@gmail.com>
2025-09-22 21:59:16 +0200

    feat : added the base for UI

--- code/UiOnionSkin3D.py
+++ code/UiOnionSkin3D.py
@@ -11,9 +11,40 @@ bl_info = {
 
 import bpy
 
-*****
-To do UI of the addon
+class OnionSkin3d(bpy.types.Panel):
+    """Creates a Panel in the Object properties window"""
+    bl_idname = "OnionSkinAddon"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_context = "objectmode"
+    bl_label = "OnionSkin3d"
+
+    def draw(self, context):
+        layout = self.layout
+
+        obj = context.object
+
+        row = layout.row()
+        row.label(text="Hello world!", icon='WORLD_DATA')
+
+        row = layout.row()
+        row.label(text="Active object is: " + obj.name)
+        row = layout.row()
+        row.prop(obj, "name")
+
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add")
 
 
+def register():
+    bpy.utils.register_class(OnionSkin3d)
 
-*****
+
+def unregister():
+    bpy.utils.unregister_class(OnionSkin3d)
+
+
+if __name__ == "__main__":
+    register()
+    
+
